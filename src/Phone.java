fc010d4f835f88f1ce7b373a4b83939906236fce
Baibhav Ghimire <baibhavghimire@MacBookAir-48D705C81183-3.local>
2018-02-03 12:49:43 -0600

    Comments added

--- src/Phone.java
+++ src/Phone.java
@@ -3,6 +3,9 @@ class Phone {
     String phoneNumber = "123456789";
     void setNumber () {
         String phoneNumber;
-        phoneNumber = "987654321";
+        phoneNumber = "987654321";  
+        
+        //Code compiles or not---Yes it does!!! :)
+        // instance variable and local variable can have same name but instance variable will override it...
     }
 }
